c93a013f4f92d97ec16e7f4eeccc6994b766a9a1
Amrutha <kamathamrutha27@gmail.com>
2024-01-30 13:39:28 +0530

    bubble_sort code

--- bubble_sort.c
+++ bubble_sort.c
@@ -20,16 +20,15 @@ int  bubble_sort(int a[],int n){
 }
 int main (){
     int n,a[100];
+    printf("\nEnter the number of elements");
     scanf("%d",&n);
+    printf("Enter the array elements");
     for(int i=0;i<=n-1;i++){
     scanf("%d",&a[i]);}
     int count=bubble_sort(a,n);
-   
+   printf("The sorted array is :\n")
      for(int i=0;i<=n-1;i++){
         printf("%d ",a[i]);}
         printf("\n%d is the number of swaps",count);
-
-    
-
-return 0;
+    return 0;
 }
