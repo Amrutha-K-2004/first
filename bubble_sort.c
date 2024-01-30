2483a123b26a8d6b70c77c53e84b0b099cc668ba
Amrutha <kamathamrutha27@gmail.com>
2024-01-30 13:27:48 +0530

    bubble_sort code

---
+++ bubble_sort.c
@@ -0,0 +1,35 @@
+//BUBBLE SORT 
+#include<stdio.h>
+#include<stdlib.h>
+int  bubble_sort(int a[],int n){
+    int flag=0,count=0;
+    for(int i=0;i<n-1;i++){
+        flag =0;
+        for(int j=0;j<n-i-1;j++){
+            if(a[j]>a[j+1]){
+                int temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
+                flag =1;
+                count++;
+            }
+            if(!flag)
+            return count;
+        }
+    }
+}
+int main (){
+    int n,a[100];
+    scanf("%d",&n);
+    for(int i=0;i<=n-1;i++){
+    scanf("%d",&a[i]);}
+    int count=bubble_sort(a,n);
+   
+     for(int i=0;i<=n-1;i++){
+        printf("%d ",a[i]);}
+        printf("\n%d is the number of swaps",count);
+
+    
+
+return 0;
+}
