c3a6ef13442c948d43774d908c39b680f0bcdeaf
Amrutha <kamathamrutha27@gmail.com>
2024-02-14 11:48:46 +0530

    .

--- tempCodeRunnerFile.c
+++
@@ -1,9 +0,0 @@
-void insert_inorder(int a[],int upper,int key){
-//     int j=upper-1;
-//     while(j>=0 && key<a[j]){
-//         a[j+1]=a[j];
-//         j--;
-//     }
-//     a[j+1]=key;
-
-// }
